511fde964cb6364980551463d484a11a1970e77d
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2022-11-07 17:26:13 +0200

    added p.lombok annotations

--- src/main/java/guru/sfg/brewery/domain/security/User.java
+++ src/main/java/guru/sfg/brewery/domain/security/User.java
@@ -1,10 +1,16 @@
 package guru.sfg.brewery.domain.security;
 
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class User {
 
@@ -15,15 +21,24 @@ public class User {
     private String password;
     private String username;
 
+    @Singular
     @ManyToMany(cascade = CascadeType.MERGE)
     @JoinTable(name="user_authority",
         joinColumns = {@JoinColumn(name="USER_ID", referencedColumnName = "ID")},
         inverseJoinColumns = {@JoinColumn(name="AUTHORITY_ID", referencedColumnName = "ID")})
     private Set<Authority> authorities;
 
+
+    @Builder.Default
     private boolean accountNonExpired = true;
+
+    @Builder.Default
     private boolean accountNonLocked = true;
+
+    @Builder.Default
     private boolean credentialsNonExpired = true;
+
+    @Builder.Default
     private boolean enabled = true;
 
 
