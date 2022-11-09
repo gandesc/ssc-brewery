4bec50afd82290af21d2d134d94833918e7ea548
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2022-11-09 16:52:08 +0200

    added authorization annotation for creating new customers

--- src/test/java/guru/sfg/brewery/web/controllers/CustomerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/CustomerControllerIT.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -22,7 +23,7 @@ public class CustomerControllerIT extends BaseIT {
     class GetCustomers {
 
         @ParameterizedTest(name = "#{index} with [{arguments}]")
-        @MethodSource("guru.sfg.brewery.web.controllers.CustomerControllerIT#getStreamAllUsers")
+        @MethodSource("guru.sfg.brewery.web.controllers.CustomerControllerIT#getStreamNotUser")
         void getCustomersAuth(String user, String pwd) throws Exception {
             mockMvc.perform(get("/customers").with(httpBasic(user, pwd)))
                     .andExpect(status().isOk());
@@ -38,6 +39,30 @@ public class CustomerControllerIT extends BaseIT {
         @Test
         void getCustomersNotLoggedIn() throws Exception {
             mockMvc.perform(get("/customers"))
+                    .andExpect(status().isUnauthorized());
+        }
+    }
+
+    @DisplayName("Create Customer")
+    @Nested
+    class CreateCustomer {
+
+        @ParameterizedTest(name = "{index} with [{arguments}]")
+        @MethodSource("guru.sfg.brewery.web.controllers.CustomerControllerIT#getStreamNotAdmin")
+        void createCustomerNotAuth(String user, String pwd) throws Exception {
+            mockMvc.perform(post("/customers/new").with(httpBasic(user, pwd)))
+                    .andExpect(status().isForbidden());
+        }
+
+        @Test
+        void createCustomerAuth() throws Exception {
+            mockMvc.perform(post("/customers/new").with(httpBasic("spring", "guru")))
+                    .andExpect(status().is3xxRedirection());
+        }
+
+        @Test
+        void createCustomerNotLoggedIn() throws Exception {
+            mockMvc.perform(post("/customers/new"))
                     .andExpect(status().isUnauthorized());
         }
     }
