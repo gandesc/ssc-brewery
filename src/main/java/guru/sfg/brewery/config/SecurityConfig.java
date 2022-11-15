b3c24cedfce538e78e2953841c6709846bdba0d9
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2022-11-15 14:20:02 +0200

    enabled csrf protection selectively

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin().and()
                 .httpBasic()
-                .and().csrf().disable();
+                .and().csrf().ignoringAntMatchers("/h2-console/**", "/api/**");
 
         http.headers().frameOptions().sameOrigin(); //for h2 frames
     }
