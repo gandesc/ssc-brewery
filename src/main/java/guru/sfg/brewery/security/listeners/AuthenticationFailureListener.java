504ab8307f275e1e669eba0177527ef885d63d3e
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2022-11-17 17:24:57 +0200

    initial impl

--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
@@ -1,5 +1,8 @@
 package guru.sfg.brewery.security.listeners;
 
+import guru.sfg.brewery.domain.security.LoginFailure;
+import guru.sfg.brewery.repositories.security.LoginFailureRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -7,25 +10,36 @@ import org.springframework.security.authentication.event.AuthenticationFailureBa
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Slf4j
 @Component
 public class AuthenticationFailureListener {
+
+    private final LoginFailureRepository loginFailureRepository;
 
     @EventListener
     public void listen(AuthenticationFailureBadCredentialsEvent event) {
         log.debug("Login failure");
 
         if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
-
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
+
+            LoginFailure.LoginFailureBuilder builder = LoginFailure.builder();
+            builder.username(token.getPrincipal().toString());
 
             log.debug("Attempted Username: " + token.getPrincipal());
 
             if (token.getDetails() instanceof WebAuthenticationDetails) {
                 WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
 
+                builder.sourceIp(details.getRemoteAddress());
+
                 log.debug("Source IP: " + details.getRemoteAddress());
             }
+
+            LoginFailure loginFailure = loginFailureRepository.save(builder.build());
+
+            log.debug("Persisted login failure with id: " + loginFailure.getId());
         }
 
     }
